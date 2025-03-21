3f02ecf9b8cba511c5ae96b99794cf56b0bc6dff
Marcos Alan <marcos38alan@gmail.com>
2025-03-21 10:30:46 -0300

    Atualizando arquivo
    Signed-off-by: Marcos Alan <marcos38alan@gmail.com>

---
+++ python_basico/inicial/variaveis03.py
@@ -0,0 +1,12 @@
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+somar = n1 + n2
+print(f"A soma entre {n1:.0f} + {n2:.0f} = {somar}")
+
+
+#tarefa:
+#criar próximas variáveis relacionadas a matemática para resolver
+#a multiplicação, subtração, divisão.
+
