b8264af90728cc994f1d4ffb48a672ef1069e6b7
Marcos Alan <marcos38alan@gmail.com>
2025-03-24 11:44:19 -0300

    Atualizando arquivo
    Signed-off-by: Marcos Alan <marcos38alan@gmail.com>

--- python_basico/inicial/variaveis04estilos.py
+++ python_basico/inicial/variaveis04estilos.py
@@ -1,9 +1,9 @@
 
 print("-"*42)
-print("Bem vindos ao formulário de contato.")
+print("\033[1;33;41mBem vindos ao sistema de cadastro.\033[m".center(55))
 print("-"*42)
 
-nome = str(input("Digite seu nome: "))
+nome = str(input("Digite seu nome: ").upper())
 idade = int(input("Digite sua idade: "))
 print("-"*60)
 print("Informe seu endereço conforme os campos abaixo.")
@@ -12,4 +12,6 @@ rua = str(input("Digite o nome da sua Rua: "))
 numero = int(input("Digite o número: "))
 complemento = str(input("COMPLEMENTO: (casa) ou (ap/número): "))
 
+print('-'*50)
 print(f"Meu nome é {nome}\nMinha idade é {idade} anos\nMeu endereço completo é {rua, numero, complemento}")
+print('-'*50)
