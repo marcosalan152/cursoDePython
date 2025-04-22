8be20841f1878efb679a1bf2211680367946245b
Marcos Alan <marcos38alan@gmail.com>
2025-04-22 09:51:40 -0300

    Atualizando arquivos
    Signed-off-by: Marcos Alan <marcos38alan@gmail.com>

--- python_basico/conclusao_de_lacos_e_condicoes/sistema_basico.py
+++ python_basico/conclusao_de_lacos_e_condicoes/sistema_basico.py
@@ -1,38 +1,44 @@
+from  time import sleep
 
 while True:
-    menu=('''
-        [1] - Cadastro de pessoas,
-        [2] - Cadastro de lista de mercado,
-        [3] - sair
+    menu = ('''
+    [1] = Cadastro pessoal,
+    [2] - Lista de compras,
+    [3] - sair
     ''')
+    print(menu)
     opc = int(input("Escolha uma opção do menu: "))
 
     if opc == 1:
-        print("Bem vindo ao cadastro de pessoas!")
-        for pessoa in range(1,11):
-            nome = str(input(f"Digite o {pessoa}º nome: "))
+        print("Bem vindos ao sistema de cadastro de pessoas")
+        for pessoas in range(1, 2):
+            nome = str(input(f"Digite o nome da {pessoas}ª pessoa: "))
 
             while True:
                 try:
                     idade = int(input("Digite a idade: "))
                 except(ValueError, TypeError):
-                    print("ERRO: Por favor digite uma idade válida.")
+                    print("ERRO: Por favor digite um valor de idade válido.")
                     continue
                 else:
                     break
 
-            sexo = str(input("Digite o sexo [Mf]: ")).strip().strip().upper()[0]
-            while sexo not in 'f/m':
-                print("ERRO: Por favor digite [m ou f] para o sexo.")
-                sexo = str(input("Digite o sexo [Mf]: ")).strip().strip().upper()[0]
-                break
+            sexo = str(input("Digite o sexo [m/f]: ")).strip().upper()[0]
+            while sexo not in 'MmFf':
+                print("ERRO: Por favor digite um sexo válido.")
+                sexo = str(input("Digite o sexo [m/f]: ")).strip().upper()[0]
 
     elif opc == 2:
-        print("Crie sua lista de mercado!")
-        produtos = str(input("Digite os itens: "))
+        print("Digite seus itens de compra")
+        itens = str(input("Digite seus itens para compras: "))
 
     elif opc == 3:
-        print("\033[7;34;41mObrigado por usar nosso sistema! Até logo!\033[m")
+        print("Obrigado por usar nosso sistema! Até logo!")
         break
     else:
-        print("ERRO: por favor escolha uma opção válida do menu.")
+        print("ERRO: Por favor escolha uma opção válida do menu.")
+
+
+    sleep(2)
+print(nome, idade, sexo)
+
